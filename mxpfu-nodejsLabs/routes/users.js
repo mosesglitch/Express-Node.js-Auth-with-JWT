e95e9fef53122f25cd4ff603a743fd20c37f9b41
mosesglitch <mosesgachuru15@gmail.com>
2023-04-14 19:21:09 +0300

    add delete and put method

--- mxpfu-nodejsLabs/routes/users.js
+++ mxpfu-nodejsLabs/routes/users.js
@@ -51,14 +51,31 @@ router.post("/", (req, res) => {
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((user) => user.email === email);
+  if (filtered_users.length > 0) {
+    let filtered_user = filtered_users[0];
+    let DOB = req.query.DOB;
+    //if the DOB has changed
+    if (DOB) {
+      filtered_user.DOB = DOB;
+    }
+    /*
+      Include code here similar to the one above for other attibutes
+      */
+    users = users.filter((user) => user.email != email);
+    users.push(filtered_user);
+    res.send(`User with the email  ${email} updated.`);
+  } else {
+    res.send("Unable to find user!");
+  }
 });
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  const email = req.params.email;
+  users = users.filter((user) => user.email != email);
+  res.send(`User with the email  ${email} deleted.`);
 });
 
 module.exports = router;
